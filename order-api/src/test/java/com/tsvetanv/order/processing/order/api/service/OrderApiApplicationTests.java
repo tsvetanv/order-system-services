e31d48a716b4194ca92849b5ef944787a3102693
tsvetanv <tsvetan.vasilev@gmail.com>
2026-01-04 16:22:57 +0200

    Added initial structure of multi-modal repo

---
+++ order-api/src/test/java/com/tsvetanv/order/processing/order/api/service/OrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsvetanv.order.processing.order.api.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
